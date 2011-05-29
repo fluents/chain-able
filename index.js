48d0e439b916ff718fc74b868f263262f4134e72
James Halliday <mail@substack.net>
2011-05-28 18:05:23 -0700

    took out up-front cloning, only fails date test

--- index.js
+++ index.js
@@ -1,71 +1,3 @@
-<<<<<<< HEAD
-var isMergeableObject = require('is-mergeable-object')
-
-function emptyTarget(val) {
-    return Array.isArray(val) ? [] : {}
-}
-
-function cloneIfNecessary(value, optionsArgument) {
-    var clone = optionsArgument && optionsArgument.clone === true
-    return (clone && isMergeableObject(value)) ? deepmerge(emptyTarget(value), value, optionsArgument) : value
-}
-
-function defaultArrayMerge(target, source, optionsArgument) {
-    var destination = target.slice()
-    source.forEach(function(e, i) {
-        if (typeof destination[i] === 'undefined') {
-            destination[i] = cloneIfNecessary(e, optionsArgument)
-        } else if (isMergeableObject(e)) {
-            destination[i] = deepmerge(target[i], e, optionsArgument)
-        } else if (target.indexOf(e) === -1) {
-            destination.push(cloneIfNecessary(e, optionsArgument))
-        }
-    })
-    return destination
-}
-
-function mergeObject(target, source, optionsArgument) {
-    var destination = {}
-    if (isMergeableObject(target)) {
-        Object.keys(target).forEach(function(key) {
-            destination[key] = cloneIfNecessary(target[key], optionsArgument)
-        })
-    }
-    Object.keys(source).forEach(function(key) {
-        if (!isMergeableObject(source[key]) || !target[key]) {
-            destination[key] = cloneIfNecessary(source[key], optionsArgument)
-        } else {
-            destination[key] = deepmerge(target[key], source[key], optionsArgument)
-        }
-    })
-    return destination
-}
-
-function deepmerge(target, source, optionsArgument) {
-    var array = Array.isArray(source)
-    var options = optionsArgument || { arrayMerge: defaultArrayMerge }
-    var arrayMerge = options.arrayMerge || defaultArrayMerge
-
-    if (array) {
-        return Array.isArray(target) ? arrayMerge(target, source, optionsArgument) : cloneIfNecessary(source, optionsArgument)
-    } else {
-        return mergeObject(target, source, optionsArgument)
-    }
-}
-
-deepmerge.all = function deepmergeAll(array, optionsArgument) {
-    if (!Array.isArray(array) || array.length < 2) {
-        throw new Error('first argument should be an array with at least two elements')
-    }
-
-    // we are sure there are at least 2 values, so it is safe to have no initial value
-    return array.reduce(function(prev, next) {
-        return deepmerge(prev, next, optionsArgument)
-    })
-}
-
-module.exports = deepmerge
-=======
 module.exports = Traverse;
 function Traverse (obj) {
     if (!(this instanceof Traverse)) return new Traverse(obj);
@@ -73,11 +5,11 @@ function Traverse (obj) {
 }
 
 Traverse.prototype.map = function (cb) {
-    return walk(this.clone(), cb);
+    return walk(this.value, cb, true);
 };
 
 Traverse.prototype.forEach = function (cb) {
-    this.value = walk(this.value, cb);
+    this.value = walk(this.value, cb, false);
     return this.value;
 };
 
@@ -157,9 +89,10 @@ Traverse.prototype.clone = function () {
     })(this.value);
 };
 
-function walk (root, cb) {
+function walk (root, cb, immutable) {
     var path = [];
     var parents = [];
+    if (immutable) root = copy(root);
     
     (function walker (node) {
         var modifiers = {};
@@ -216,7 +149,7 @@ function walk (root, cb) {
         state.notRoot = !state.isRoot;
         
         // use return values to update if defined
-        var ret = cb.call(state, node);
+        var ret = cb.call(state, state.node);
         if (ret !== undefined && state.update) state.update(ret);
         if (modifiers.before) modifiers.before.call(state, state.node);
         
@@ -230,7 +163,11 @@ function walk (root, cb) {
                 
                 if (modifiers.pre) modifiers.pre.call(state, state.node[key], key);
                 
-                var child = walker(state.node[key]);
+                var child = walker(
+                    immutable ? copy(state.node[key]) : state.node[key]
+                );
+                if (immutable) state.node[child.key] = child.node;
+                
                 child.isLast = i == keys.length - 1;
                 child.isFirst = i == 0;
                 
@@ -256,4 +193,17 @@ Object.keys(Traverse.prototype).forEach(function (key) {
         return t[key].apply(t, args);
     };
 });
->>>>>>> completely rewrote Traverse, deleted hash.js and web.js
+
+function copy (node) {
+    if (Array.isArray(node)) {
+        return node.slice();
+    }
+    else if (typeof node === 'object') {
+        var cp = Object.create(node.__proto__);
+        Object.keys(node).forEach(function (key) {
+            cp[key] = node[key];
+        });
+        return cp;
+    }
+    else return node;
+}
