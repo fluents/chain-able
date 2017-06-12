34aadeeb4fb4c4c7ffe14ea6c40e1f279b743547
Arete Code <aretecode@gmail.com>
2017-06-12 15:17:47 -0700

    👾 dopemerge simplified

--- src/deps/dopemerge.js
+++ src/deps/dopemerge.js
@@ -1,42 +1,39 @@
-let _debug = true
+const isPureObj = require('./is/pureObj')
+const isArray = require('./is/array')
+const toS = require('./is/toS')
+
+const ezType = x => (isArray(x) ? 'array' : typeof x)
 
 // @TODO convert forEach for faster loops
-// import deepmerge from 'deepmerge'
 function isMergeableObj(val) {
-  var strType = Object.prototype.toString.call(val)
-
   return (
     // not null object
-    val !== null &&
-    typeof val === 'object' &&
+    isPureObj(val) &&
     // object toString is not a date or regex
-    strType !== '[object RegExp]' &&
-    strType !== '[object Date]'
+    !['[object RegExp]', '[object Date]'].includes(toS(val))
   )
 }
 
 function emptyTarget(val) {
-  return Array.isArray(val) ? [] : {}
+  return isArray(val) ? [] : {}
 }
-
 function cloneIfNeeded(value, optsArg) {
-  var clone = optsArg && optsArg.clone === true
-  return clone && isMergeableObj(value)
+  return optsArg.clone === true && isMergeableObj(value)
     ? deepmerge(emptyTarget(value), value, optsArg)
     : value
 }
 
 function defaultArrayMerge(target, source, optsArg) {
   var destination = target.slice()
-  source.forEach((e, i) => {
+  source.forEach((v, i) => {
     if (typeof destination[i] === 'undefined') {
-      destination[i] = cloneIfNeeded(e, optsArg)
+      destination[i] = cloneIfNeeded(v, optsArg)
     }
-    else if (isMergeableObj(e)) {
-      destination[i] = deepmerge(target[i], e, optsArg)
+    else if (isMergeableObj(v)) {
+      destination[i] = deepmerge(target[i], v, optsArg)
     }
-    else if (target.indexOf(e) === -1) {
-      destination.push(cloneIfNeeded(e, optsArg))
+    else if (target.indexOf(v) === -1) {
+      destination.push(cloneIfNeeded(v, optsArg))
     }
   })
   return destination
@@ -61,33 +58,18 @@ function mergeObj(target, source, optsArg) {
 }
 
 function deepmerge(target, source, optsArg) {
-  var options = optsArg || {arrayMerge: defaultArrayMerge}
-  var arrayMerge = options.arrayMerge || defaultArrayMerge
-
-  if (Array.isArray(source)) {
-    return Array.isArray(target)
+  if (isArray(source)) {
+    const {arrayMerge} = optsArg
+    return isArray(target)
       ? arrayMerge(target, source, optsArg)
       : cloneIfNeeded(source, optsArg)
   }
-  else {
-    return mergeObj(target, source, optsArg)
-  }
+
+  // else
+  return mergeObj(target, source, optsArg)
 }
 
 // unused
-// deepmerge.all = function deepmergeAll(array, optsArg) {
-//   if (!Array.isArray(array) || array.length < 2) {
-//     throw new Error(
-//       'first argument should be an array with at least two elements'
-//     )
-//   }
-//
-//   // we are sure there are at least 2 values, so it is safe to have no initial value
-//   return array.reduce((prev, next) => {
-//     return deepmerge(prev, next, optsArg)
-//   })
-// }
-
 // @TODO options for merging arr, and on any type combo
 // const todoOpts = {
 //   // when: { left(cb), right(cb) }
@@ -109,6 +91,7 @@ function eqCurry(types) {
 
 function getDefaults() {
   return {
+    arrayMerge: defaultArrayMerge,
     stringToArray: true,
     boolToArray: false,
     boolAsRight: true,
@@ -117,41 +100,21 @@ function getDefaults() {
   }
 }
 
-// require('fliplog').fmtobj({types, options, obj1, obj2}).echo(false)
-// require('fliplog')
-//   .data({
-//     boolbool: eq(['boolean', 'boolean']),
-//     strstr: eq(['string', 'string']),
-//     arrstr: eq(['array', 'string']),
-//     strarr: eq(['string', 'array']),
-//   })
-//   .echo()
-
-function getOpts(opts) {
-  const defaults = getDefaults()
-  const options = Object.assign(defaults, opts)
-  return options
-}
-
 // eslint-disable-next-line complexity
 function dopemerge(obj1, obj2, opts = {}) {
   // if they are identical, fastest === check
   if (obj1 === obj2) return obj1
 
   // setup options
-  const {ignoreTypes, stringToArray, boolToArray} = getOpts(opts)
+  const options = Object.assign(getDefaults(), opts)
+  const {ignoreTypes, stringToArray, boolToArray, clone} = options
 
-  // setup vars
-  let type1 = typeof obj1
-  let type2 = typeof obj2
-  if (Array.isArray(obj1)) type1 = 'array'
-  if (Array.isArray(obj2)) type2 = 'array'
-  const types = [type1, type2]
+  const types = [ezType(obj1), ezType(obj2)]
 
   // check one then check the other
   // @TODO might want to push undefined null nan into array but...
-  if (ignoreTypes.includes(type1) === true) return obj2
-  if (ignoreTypes.includes(type2) === true) return obj1
+  if (ignoreTypes.includes(types[0]) === true) return obj2
+  if (ignoreTypes.includes(types[1]) === true) return obj1
 
   const eq = eqCurry(types)
 
@@ -164,13 +127,13 @@ function dopemerge(obj1, obj2, opts = {}) {
       return stringToArray ? [obj1, obj2] : obj1 + obj2
     }
     case eq(['array', 'string']): {
-      return obj1.concat([obj2])
+      return (clone ? obj1.slice(0) : obj1).concat([obj2])
     }
     case eq(['string', 'array']): {
-      return obj2.concat([obj1])
+      return (clone ? obj2.slice(0) : obj2).concat([obj1])
     }
     default: {
-      return deepmerge(obj1, obj2)
+      return deepmerge(obj1, obj2, options)
     }
   }
 }
