9d04e8cdd725b705fc217918feae4b6f485a8fc8
Arete Code <aretecode@gmail.com>
2017-06-12 15:33:15 -0700

    🖇🔬 utils/is tests

---
+++ test/is/primatives.js
@@ -0,0 +1,31 @@
+const test = require('ava')
+const {isReal, isBoolean, isNumber, isString} = require('./')
+
+test('should work for undefined', t => {
+  t.false(isReal(undefined))
+})
+
+test('should work for null', t => {
+  t.false(isReal(null))
+})
+
+test('should work for booleans', t => {
+  t.true(isBoolean(true))
+  t.true(isBoolean(false))
+  t.true(isBoolean(new Boolean(true)))
+})
+
+test('should work for numbers', t => {
+  t.true(isNumber(42))
+  t.true(isNumber(new Number(42)))
+})
+
+test('should work for strings', t => {
+  t.true(isString('str'))
+  t.true(isString(new String('str')))
+})
+
+test('should work for template strings', t => {
+  var str = `Welcome buddy`
+  t.true(isString(str))
+})
