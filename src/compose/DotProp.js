693332f6b32617d1d2f234319597ab6be57ea4c4
Arete Code <aretecode@gmail.com>
2017-06-12 15:25:25 -0700

    🎁⚒ dot-prop implemented with .get .set .has .delete in 🎼 compose/DotProp

--- src/compose/DotProp.js
+++ src/compose/DotProp.js
@@ -2,27 +2,83 @@
  * @since 2.0.0
  */
 const dot = require('../deps/dot-prop')
-const isDot = k => typeof k === 'string' && k.includes('.')
+const isDot = require('../deps/is/dot')
+const getPathSegments = require('../deps/dot-segments')
+
+/**
+ * @desc checks if this._dot != false & isDot(key)
+ * @see this.dot
+ * @param  {string} key
+ * @param  {DotProp} thisArg
+ * @return {boolean}
+ */
+const shouldDot = (key, thisArg) => {
+  return thisArg._dot !== false && isDot(key)
+}
 
 module.exports = (SuperClass, opts) => {
   return class DotProp extends SuperClass {
+    /**
+     * @param  {Boolean} [useDot=true]
+     * @return {DotProp} @chainable
+     */
+    dot(useDot = true) {
+      this._dot = useDot
+      return this
+    }
+
+    /**
+     * @inheritdoc
+     * @see .dot
+     * @desc since we have a map,
+     *       we need to ensure the first property is available
+     *       otherwise we have an empty map.entries obj
+     *       which does nothing by reference
+     */
     set(key, val) {
-      return isDot(key)
-        ? super.set(
-            key.split('.').shift(),
-            dot.set(super.entries(this), key, val) || val
-          )
-        : super.set(key, val)
+      if (shouldDot(key, this)) {
+        const prop = key.split('.').shift()
+
+        // we already know it is .dot, call super instead
+        if (!super.has(prop)) super.set(prop, {})
+
+        // spread
+        const data = super.entries()
+
+        // set on the spread data
+        dot.set(data, key, val)
+
+        // is already by ref, but be extra safe
+        return super.set(prop, data[prop])
+      }
+      return super.set(key, val)
     }
+    /**
+     * @inheritdoc
+     * @see .dot
+     */
     get(key) {
-      return isDot(key) ? dot.get(super.entries(this), key) : super.get(key)
+      return shouldDot(key, this)
+        ? dot.get(super.entries(), key)
+        : super.get(key)
     }
+    /**
+     * @inheritdoc
+     * @see .dot
+     */
     has(key) {
-      return isDot(key) ? dot.has(super.entries(this), key) : super.has(key)
+      return shouldDot(key, this)
+        ? dot.has(super.entries(), key)
+        : super.has(key)
     }
+
+    /**
+     * @inheritdoc
+     * @see .dot
+     */
     delete(key) {
-      return isDot(key)
-        ? dot.delete(super.entries(this), key)
+      return shouldDot(key, this)
+        ? dot.delete(super.entries(), key)
         : super.delete(key)
     }
 
@@ -56,17 +112,14 @@ module.exports = (SuperClass, opts) => {
      * @return {Object}
      */
     _dotter(name) {
-      let accessor = name
-      let first = name
-      let hasDot = isDot(name)
       let value
-
-      if (hasDot) {
-        accessor = name.split('.')
-        first = accessor.shift()
-      }
-
       const dotted = {}
+      const hasDot = isDot(name)
+
+      // @NOTE: shifting the first one off will change accessor
+      // @example moose.simple -> first: 'moose', accessor: ['simple']
+      const accessor = hasDot ? getPathSegments(name) : false
+      const first = accessor ? accessor.shift() : false
 
       dotted.dotted = cb => {
         if (hasDot === false) return dotted
