c37a78f2389e486f97230c529f89b9c791be7881
Arete Code <aretecode@gmail.com>
2017-06-21 23:31:47 -0700

    ⚡🎯 matcher loops 🖇 typechecks ℹ️️ docs 💸 recache

--- src/deps/matcher.js
+++ src/deps/matcher.js
@@ -2,37 +2,56 @@
 const toarr = require('./to-arr')
 const toRegExp = require('./to-regexp')
 const isMatcher = require('./is/matcher')
-
-// const toarr = x => [].concat(x)
-// const escapeStringRegexp = str => str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
-// const toRegExp = pattern => escapeStringRegexp(pattern).replace(/\\\*/g, '.*')
-// const isMatcher = x =>
-//   typeof x === 'function' || x instanceof Function || x instanceof RegExp
+const ObjectAssign = require('./util/assign')
+const cache = require('./cache')
 
 const m = {}
 
+/**
+ * @since 3.0.0
+ * @param  {Array<string> | string | Function | RegExp} pattern
+ * @param  {boolean | undefined} shouldNegate
+ * @param  {boolean | undefined} alphaOmega
+ * @return {Array<string> | string | Function | RegExp} matchable
+ */
 m.make = (pattern, shouldNegate, alphaOmega) => {
-  if (isMatcher(pattern) && !pattern.test) pattern.test = pattern
-  if (isMatcher(pattern)) return pattern
+  if (cache.has(pattern)) return cache.get(pattern)
 
-  let negated = pattern[0] === '!'
-  if (negated) pattern = pattern.slice(1)
-  pattern = toRegExp(pattern)
+  let matchable = pattern
+  if (isMatcher(matchable) && !matchable.test) matchable.test = matchable
+  if (isMatcher(matchable)) return matchable
 
-  if (negated && shouldNegate) pattern = `(?!${pattern})`
-  if (alphaOmega) pattern = `^${pattern}$`
+  let negated = matchable[0] === '!'
+  if (negated) matchable = matchable.slice(1)
+  matchable = toRegExp(matchable)
 
-  let re = new RegExp(`${pattern}`, 'i')
-  re.negated = negated
-  return re
+  if (negated && shouldNegate) matchable = `(?!${matchable})`
+  if (alphaOmega) matchable = `^${matchable}$`
+
+  matchable = new RegExp(`${matchable}`, 'i')
+  matchable.negated = negated
+
+  cache.set(pattern, matchable)
+  return matchable
 }
 
+/**
+ * @since 3.0.0
+ * @param  {Array<string> | string} inputs
+ * @param  {Array<string> | string | Function | RegExp} patterns
+ * @param  {boolean | undefined} shouldNegate
+ * @param  {boolean | undefined} alphaOmega
+ * @return {Array<any>}
+ */
 m.matcher = (inputs, patterns, shouldNegate, alphaOmega) => {
   patterns = toarr(patterns).map(p => m.make(p, shouldNegate, alphaOmega))
+  inputs = toarr(inputs)
+
   const firstNegated = patterns[0].negated
   const matchesToReturn = []
 
-  toarr(inputs).forEach(input => {
+  for (var i = 0; i < inputs.length; i++) {
+    const input = inputs[i]
     // If first pattern is negated we include everything to match user expectation
     let matches = firstNegated
     for (let j = 0; j < patterns.length; j++) {
@@ -42,9 +61,9 @@ m.matcher = (inputs, patterns, shouldNegate, alphaOmega) => {
     }
 
     if (matches) matchesToReturn.push(input)
-  })
+  }
 
   return matchesToReturn
 }
 
-module.exports = Object.assign(m.matcher, m)
+module.exports = ObjectAssign(m.matcher, m)
