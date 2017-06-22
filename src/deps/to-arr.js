4a4a1b9a5dbfae50d04d57f6d800a6af05f2de61
Arete Code <aretecode@gmail.com>
2017-06-21 23:33:14 -0700

    ℹ️️ docblock 🖇 typecheck utils 🛁 clean to-arr

--- src/deps/to-arr.js
+++ src/deps/to-arr.js
@@ -1,20 +1,26 @@
 // const isReal = require('./is/real')
+// const isString = require('./is/string')
+const isArray = require('./is/array')
+const isSet = require('./is/set')
+const isMap = require('./is/map')
+const isIterator = require('./is/iterator')
+const ArrayFrom = require('./util/from')
 
+/**
+ * @since 0.0.1
+ * @param  {any} ar
+ * @return {Array}
+ */
 module.exports = function(ar) {
   // @NOTE: !'' === true
   if (typeof ar === 'string') return ar.includes(',') ? ar.split(',') : [ar]
-  if (!ar) return [ar]
-  if (Array.isArray(ar)) return ar
-  // @NOTE: .values is also on `Object`...
-  if (ar instanceof Set || ar instanceof Map || ar.values) {
-    return Array.from(ar.values(ar))
+  else if (!ar) return [ar]
+  else if (isArray(ar)) return ar
+  else if (isSet(ar) || isMap(ar) || ar.values) {
     // @NOTE: when using `new Set().values`... no forEach o.o
-    // const vals = []
-    // ar.values().forEach(v => vals.push(v))
-    // return vals
+    // @NOTE: .values is also on `Object`...
+    return ArrayFrom(ar.values(ar))
   }
-
-  return [ar]
+  else if (isIterator(ar)) return ArrayFrom(ar)
+  else return [ar]
 }
-
-// module.exports.slice = Array.prototype.slice.call.bind(Array.prototype.slice)
