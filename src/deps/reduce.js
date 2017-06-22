c871d08f56ad13774af823d36307d3fb1b668bfd
Arete Code <aretecode@gmail.com>
2017-06-22 08:05:05 -0700

    🖇ℹ️️ docs for reduce util

--- src/deps/reduce.js
+++ src/deps/reduce.js
@@ -1,12 +1,22 @@
-module.exports = map => {
-  const entries = Array.from(map.entries())
+const ArrayFrom = require('./util/from')
 
+/**
+ * @since 4.0.0
+ * @desc Map -> Object
+ * @param  {Map} map
+ * @return {Object}
+ */
+module.exports = map => {
   let reduced = {}
-  if (entries.length !== 0) {
+
+  // only need to do this if we actually have values in our Map
+  if (map.size !== 0) {
+    const entries = ArrayFrom(map.entries())
     reduced = entries.reduce((acc, [key, value]) => {
       acc[key] = value
       return acc
     }, {})
   }
+
   return reduced
 }
