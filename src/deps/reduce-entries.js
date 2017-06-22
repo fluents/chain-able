5338b1e225b0483a48794b1e0114689023358962
Arete Code <aretecode@gmail.com>
2017-06-22 08:04:40 -0700

    🖇 reduce util

---
+++ src/deps/reduce-entries.js
@@ -0,0 +1,29 @@
+const isFunction = require('./is/function')
+const ignored = require('./ignored')
+const ObjectKeys = require('./util/keys')
+const ObjectAssign = require('./util/assign')
+
+/**
+ * @since 4.0.0
+ * @see ChainedMap
+ * @param {Object | any} reduced
+ * @return {Function} Function(values: Object)
+ */
+module.exports = reduced => self => {
+  const keys = ObjectKeys(self)
+
+  for (let k = 0; k < keys.length; k++) {
+    const key = keys[k]
+
+    if (ignored(key)) {
+      continue
+    }
+
+    const val = self[key]
+    if (val && isFunction(val.entries)) {
+      ObjectAssign(reduced, {[key]: val.entries(true) || {}})
+    }
+  }
+
+  return reduced
+}
