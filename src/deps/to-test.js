20315a14523f8f15c16437450e94224f53ae2fa8
Arete Code <aretecode@gmail.com>
2017-06-21 23:32:46 -0700

    ℹ️️ docblock 🖇 typecheck utils 🛁 clean to-test

--- src/deps/to-test.js
+++ src/deps/to-test.js
@@ -1,15 +1,17 @@
 const esc = require('./escape-string-regex')
+const isString = require('./is/string')
+const isFunction = require('./is/function')
 
-module.exports = (key, arg1, arg2) => {
-  const type = typeof key
-  // log
-  //   .dim('testing keys')
-  //   .data({test, arg1, matched: test.test(arg1)})
-  //   .echo(debug)
-  if (type === 'string') {
-    const test = new RegExp(esc(key))
-    return !!test.test(arg1)
-  }
-  if (type === 'function' && !key.test) return !!key(arg1)
-  return !!key.test(arg1, arg2)
+/**
+ * @since  3.0.0
+ * @NOTE   as else-if for easier ternary uglification
+ * @param  {Matchable} matchable
+ * @param  {any} arg1
+ * @param  {any} arg2
+ * @return {boolean}
+ */
+module.exports = (matchable, arg1, arg2) => {
+  if (isString(matchable)) return !!new RegExp(esc(matchable)).test(arg1)
+  else if (isFunction(matchable) && !matchable.test) return !!matchable(arg1)
+  else return !!matchable.test(arg1, arg2)
 }
