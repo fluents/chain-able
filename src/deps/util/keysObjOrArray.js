e564ce22bcc7401a175dfcfeacf81099b537163b
Arete Code <aretecode@gmail.com>
2017-07-22 00:51:18 -0700

    🖇 util 🆕 localGlobal 🆙⛑ update & safety ℹ️️ docs
      - 🆙 utils/ 🕴 exports
      - 🌏 localGlobal (window || global)
      - 🔢 lengthFromZero
        - ℹ️️ docs
        - 😡 use lengthFromZero in `argumentor`
      - ⛑ hasOwnProperty add `isNill` safety

--- src/deps/util/keysObjOrArray.js
+++ src/deps/util/keysObjOrArray.js
@@ -5,21 +5,26 @@ const lengthFromZero = require('./lengthFromZero')
 
 /**
  * Creates an array of the own enumerable property names of `object`.
- *
  * **Note:** Non-object values are coerced to objects. See the
  * [ES spec](http://ecma-international.org/ecma-262/7.0/#sec-object.keys)
  * for more details.
  *
  * @since 0.1.0
  * @category Object
+ * @name keysObjOrArray
  *
- * @param {Object} object The object to query.
+ * @param {Object} obj The object to query.
  * @return {Array} Returns the array of property names.
  *
+ * @see deps/util/lengthFromZero
  * @see deps/util/props
  * @see values, valuesIn
- * @see https://github.com/lodash/lodash/blob/master/.internal/getAllKeys.js
- * @see https://github.com/lodash/lodash/blob/master/keys.js
+ *
+ * {@link https://github.com/lodash/lodash/blob/master/keys.js lodash-keys}
+ * {@link https://github.com/lodash/lodash/blob/master/.internal/getAllKeys.js lodash-get-all-keys}
+ * @see {@link lodash-keys}
+ * @see {@link lodash-get-all-keys}
+ *
  * @TODO https://github.com/lodash/lodash/blob/master/.internal/arrayLikeKeys.js
  *
  * @example
@@ -38,8 +43,6 @@ const lengthFromZero = require('./lengthFromZero')
  * // => ['0', '1']
  *
  */
-
-
 module.exports = function keys(obj) {
   return isArray(obj)
     ? new Array(lengthFromZero(obj))
--- src/deps/util/lengthFromZero.js
+++ src/deps/util/lengthFromZero.js
@@ -1,2 +1,23 @@
+/**
+ * when length > 1, use length-1
+ * otherwise, when length == 1, use 0
+ * default, use length
+ *
+ *
+ * @TODO lense to use an object, or transform it to one with .length?
+ *  const len = prop('length')
+ *  // when isObj, use len, otherwise, value
+ *  const coerceLength = lense([isObj, len])
+ *
+ * @param {Array | Object | number} obj with length
+ * @return {number} obj length from 0
+ *
+ * @example
+ *
+ *  lengthFromZero([1])        //=> 1
+ *  lengthFromZero([])         //=> 0
+ *  lengthFromZero([1, 2, 3])  //=> 2
+ *
+ */
 module.exports = obj =>
   (obj.length > 1 ? obj.length - 1 : obj.length === 1 ? 1 : 0)
