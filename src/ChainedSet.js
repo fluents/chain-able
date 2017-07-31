633be8d16e39cbc1a463c132fb495c57bf634c91
Arete Code <aretecode@gmail.com>
2017-07-31 03:23:04 -0700

    ChainedSet - fix casting, convert to hybrid-class
    - ⚒ fix keys casting in ChainedSet (was casting iterator needed to be arrfrom)
    - `ChainedSet` as hybrid class

--- src/ChainedSet.js
+++ src/ChainedSet.js
@@ -1,7 +1,6 @@
 const Chainable = require('./Chainable')
 const toarr = require('./deps/to-arr')
-const newSet = require('./deps/construct/set')
-const arrayOfIndexes = require('./deps/cast/arrayOfIndexes')
+const arrayOfIndexes = require('./deps/array/arrayOfIndexes')
 
 /**
  * @class
@@ -38,102 +37,106 @@ class ChainedSet extends Chainable {
     super(parent)
     this.store = new Set()
   }
+}
 
-  /**
-   * @version 5.0.0 <- moved into ChainedMapBase & ChainedSet for less monomorphic usage
-   * @since 5.0.0-beta.6
-   * @return {Array<number | string | *>} keys
-   *
-   * @example
-   *    Chain.set('eh', 1).keys()
-   *    //=> ['eh']
-   */
-  keys() {
-    return arrayOfIndexes(this.store.size)
+/**
+ * @version 5.0.0 <- moved into ChainedMapBase & ChainedSet for less monomorphic usage
+ * @since 5.0.0-beta.6
+ * @memberOf ChainedSet
+ *
+ * @return {Array<number | string | *>} keys
+ *
+ * @example
+ *
+ *    Chain.set('eh', 1).keys()
+ *    //=> ['eh']
+ *
+ */
+ChainedSet.prototype.keys = function() {
+  return arrayOfIndexes(this.store.size)
+}
+
+
+/**
+ * @desc appends a new element with a specified value to the end of the .store
+ * @since 0.4.0
+ * @memberOf ChainedSet
+ *
+ * @param {any} value any value to add to **end** of the store
+ * @return {ChainedSet} @chainable
+ *
+ * {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/add mozilla-set-add}
+ * {@link https://github.com/lodash/lodash/blob/master/.internal/addSetEntry.js#L9 lodash-add-set-entry}
+ * @see {@link mozilla-set-add}
+ * @see {@link lodash-add-set-entry}
+ *
+ * @example
+ *
+ *   const people = new ChainedSet()
+ *   people
+ *     .add('sam')
+ *     .add('sue')
+ *
+ *   for (let name of people) console.log(name)
+ *   //=> sam, sue
+ *
+ */
+ChainedSet.prototype.add = function(value) {
+  this.store.add(value)
+  return this
+}
+
+/**
+ * @desc inserts the value at the **beginning** of the Set
+ * @since 0.4.0
+ * @memberOf ChainedSet
+ *
+ * @param {any} value any value to add to **beginning** the store
+ * @return {ChainedSet} @chainable
+ *
+ * @example
+ *
+ *   const people = new ChainedSet()
+ *   people
+ *     .add('sue')
+ *     .prepend('first')
+ *
+ *   for (let name of people) console.log(name)
+ *   //=> first, sue
+ *
+ */
+ChainedSet.prototype.prepend = function(value) {
+  this.store = new Set([value].concat(this.values()))
+  return this
+}
+
+/**
+ * @desc merge any Array/Set/Iteratable/Concatables into the array, at the end
+ * @since 0.4.0
+ * @memberOf ChainedSet
+ *
+ * @param {Array | Set | Concatable} arr values to merge in and append
+ * @return {ChainedSet} @chainable
+ *
+ * @example
+ *
+ *   const people = new ChainedSet()
+ *   people
+ *     .add('sam')
+ *     .add('sue')
+ *     .prepend('first')
+ *     .merge(['merged'])
+ *
+ *   for (let name of people) console.log(name)
+ *   //=> first, sam, sue, merged
+ *
+ */
+ChainedSet.prototype.merge = function(arr) {
+  const mergeable = toarr(arr)
+  for (let i = 0; i < mergeable.length; i++) {
+    this.store.add(mergeable[i])
   }
-
-
-  /**
-   * @desc appends a new element with a specified value to the end of the .store
-   * @memberOf ChainedSet
-   * @since 0.4.0
-   *
-   * @param {any} value any value to add to **end** of the store
-   * @return {ChainedSet} @chainable
-   *
-   * {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/add mozilla-set-add}
-   * {@link https://github.com/lodash/lodash/blob/master/.internal/addSetEntry.js#L9 lodash-add-set-entry}
-   * @see {@link mozilla-set-add}
-   * @see {@link lodash-add-set-entry}
-   *
-   * @example
-   *
-   *   const people = new ChainedSet()
-   *   people
-   *     .add('sam')
-   *     .add('sue')
-   *
-   *   for (let name of people) console.log(name)
-   *   //=> sam, sue
-   *
-   */
-  add(value) {
-    this.store.add(value)
-    return this
-  }
-
-  /**
-   * @desc inserts the value at the **beginning** of the Set
-   * @memberOf ChainedSet
-   * @since 0.4.0
-   *
-   * @param {any} value any value to add to **beginning** the store
-   * @return {ChainedSet} @chainable
-   *
-   * @example
-   *
-   *   const people = new ChainedSet()
-   *   people
-   *     .add('sue')
-   *     .prepend('first')
-   *
-   *   for (let name of people) console.log(name)
-   *   //=> first, sue
-   *
-   */
-  prepend(value) {
-    this.store = new Set([value].concat(super.values()))
-    return this
-  }
-
-  /**
-   * @desc merge any Array/Set/Iteratable/Concatables into the array, at the end
-   * @since 0.4.0
-   * @memberOf ChainedSet
-   *
-   * @param {Array | Set | Concatable} arr values to merge in and append
-   * @return {ChainedSet} @chainable
-   *
-   * @example
-   *
-   *   const people = new ChainedSet()
-   *   people
-   *     .add('sam')
-   *     .add('sue')
-   *     .prepend('first')
-   *     .merge(['merged'])
-   *
-   *   for (let name of people) console.log(name)
-   *   //=> first, sam, sue, merged
-   *
-   */
-  merge(arr) {
-    const mergeable = toarr(arr)
-    for (let i = 0; i < mergeable.length; i++) {
-      this.store.add(mergeable[i])
-    }
-    return this
-  }
+  return this
 }
 
 module.exports = ChainedSet
