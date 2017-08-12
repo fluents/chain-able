2e72341fd0489be4add954454f9d2bd22e4cc3d2
Arete Code <aretecode@gmail.com>
2017-08-12 14:25:33 -0700

    adjust linting func-style for rollup + uglify

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,22 @@
 module.exports = {
-  extends: ['chain-able'],
+  extends: ["chain-able"],
+  globals: {
+    eq: true
+  },
   // ext: ['.ts', '.js', '.jsx', '.tsx', 'ts'],
   rules: {
-    'prefer-rest-params': 'OFF',
-    'jsdoc/require-example': 'error',
-    'jsdoc/require-param': 'error',
-    'jsdoc/require-param-description': 'error',
-    'jsdoc/require-returns-type': 'error',
-    'import/max-dependencies': 'OFF',
+    "prefer-rest-params": "OFF",
+    "jsdoc/require-example": "error",
+    "jsdoc/require-param": "error",
+    "jsdoc/require-param-description": "error",
+    "jsdoc/require-returns-type": "error",
+    "import/max-dependencies": "OFF",
+
+    // @NOTE this is because using rollup,
+    // bundling functions with names...
+    // gives variables assigned to functions that also have names
+    "func-style": "OFF"
+
     // 'dot-notation': ["error", { "allowKeywords": false }],
     /* eslint 'dot-notation': ["error", { "allowKeywords": false }] */
 
@@ -21,9 +30,9 @@ module.exports = {
     // 'inferno/display-name': 'OFF',
     // 'inferno/display-no-depreciated': 'OFF',
   },
-  plugins: ['jest', 'jsdoc'],
+  plugins: ["jest", "jsdoc"],
   globals: {
     expect: true,
-    test: true,
-  },
-}
+    test: true
+  }
+};
