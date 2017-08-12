d8d5b98444291eaab47ebfaa200ed8f94ba2511b
Arete Code <aretecode@gmail.com>
2017-08-12 14:25:47 -0700

    babel assign polyfill (comment)

--- src/deps/util/assign.js
+++ src/deps/util/assign.js
@@ -96,3 +96,16 @@ module.exports = Object.assign
 //
 //   return to
 // }
+
+// babel
+// function(target) {
+//   for (var i = 1; i < arguments.length; i++) {
+//     var source = arguments[i]
+//     for (var key in source) {
+//       if (Object.prototype.hasOwnProperty.call(source, key)) {
+//         target[key] = source[key]
+//       }
+//     }
+//   }
+//   return target
+// }
