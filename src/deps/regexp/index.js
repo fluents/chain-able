12924978e6a0841513b184a5756819f9e957149c
Arete Code <aretecode@gmail.com>
2017-08-12 14:42:30 -0700

    export matchables

--- src/deps/regexp/index.js
+++ src/deps/regexp/index.js
@@ -4,7 +4,17 @@
 //     return (/^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i).test(value)
 //   },
 // }
-const url = require('./url')
+const url = require('./matchUrl')
+const matchDeepProp = require('./matchDeepProp')
+const matchRegExp = require('./regexpMatch')
+const matchPlainProp = require('./matchPlainProp')
+const matchUnsigned = require('./matchUnsigned')
+const matchInteger = require('./matchInteger')
+const matchNative = require('./matchNative')
+const matchRegExpEscapable = require('./matchRegExpEscapable')
+const matchTypedTag = require('./matchTypedTag')
+const matchUnicode = require('./matchUnicode')
+const matchHex = require('./matchHex')
 
 // dateString match m/d/yy and mm/dd/yyyy, allowing any combination of one or two digits for the day and month, and two or four digits for the year
 // eppPhone match extensible provisioning protocol format
