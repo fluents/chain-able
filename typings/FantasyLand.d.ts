1f69fb4da3d9d70c033fccbf641a8003d2e3e10f
Arete Code <aretecode@gmail.com>
2017-08-12 14:24:24 -0700

    nary type declarations

--- typings/FantasyLand.d.ts
+++ typings/FantasyLand.d.ts
@@ -41,6 +41,20 @@ export interface Plus<T> extends Alt<T> {
 export interface Alternative<T> extends Plus<T>, Applicative<T> {
 }
 
+/**
+ * Generate a function that accepts a number of arguments.
+ */
+export function nary <U> (length: 0, fn: () => U): () => U
+export function nary <T, U> (length: 1, fn: (arg: T) => U): (arg: T) => U
+export function nary <T1, T2, U> (length: 2, fn: (arg1: T1, arg2: T2) => U): (arg1: T1, arg2: T2) => U
+export function nary <T1, T2, T3, U> (length: 3, fn: (arg1: T1, arg2: T2, arg3: T3) => U): (arg1: T1, arg2: T2, arg3: T3) => U
+export function nary <T1, T2, T3, T4, U> (length: 4, fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4) => U): (arg1: T1, arg2: T2, arg3: T3, arg4: T4) => U
+export function nary <T1, T2, T3, T4, T5, U> (length: 5, fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5) => U): (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5) => U
+export function nary <T1, T2, T3, T4, T5, T6, U> (length: 6, fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6) => U): (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6) => U
+export function nary <TAll, U> (length: number, fn: (args: TAll[]) => U): (args: TAll[]) => U
+export function nary (length: number, fn: (...args: any[]) => any): (...args: any[]) => any {
+}
+
 export interface Foldable<T> {
     reduce<U>(fn: (u: U, t: T) => U, u: U): U;
 }
